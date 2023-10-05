042ecaa630310fdad3fdd8a16075d01a9fde5ba3
Jaemin Eun <tomy000120@naver.com>
2023-10-05 15:06:16 +0900

    2nd week set

--- 2nd/Adj_List.py
+++ 2nd/Adj_List.py
@@ -1,41 +1,47 @@
 def print_Adjlist(graph):
     print("==========================")
     for i in range(len(graph)):
-        print(i+1,end=' -> ')
-        for el in graph[i] :
-            print(el+1,end=', ')
+        print(i + 1, end=" -> ")
+        for el in graph[i]:
+            print(el + 1, end=", ")
         print()
     print("==========================")
 
-class Node : #Link List에 사용하는 노드클래스, 참고만
-    #생성자
-    def __init__(self,n_data,link=None):
+
+class Node:  # Link List에 사용하는 노드클래스, 참고만
+    # 생성자
+    def __init__(self, n_data, link=None):
         self.data = n_data
         self.link = link
-    
+
+
 # 파이썬의 리스트는 말단삽입(append)를 지원하므로
 # 2차원 리스트로 인접리스트 표현을 구현할 수 있다.
 # 간선만 나타내므로, 희소그래프에서 유용하다.
 
-#case1
-#삽입되는 순서는 중요하지 않음
-G = [[1,4], #1번노드
-     [0,2,4], #2
-     [1,3], #3
-     [2,4,5], #4
-     [0,1,3], #5
-     [3]] #6
+# case1
+# 삽입되는 순서는 중요하지 않음
+G = [
+    [1, 4],  # 1번노드
+    [0, 2, 4],  # 2
+    [1, 3],  # 3
+    [2, 4, 5],  # 4
+    [0, 1, 3],  # 5
+    [3],
+]  # 6
 
 print_Adjlist(G)
 
-#case2
-N = 6 #노드 개수
+# case2
+N = 6  # 노드 개수
 Graph = [[] for _ in range(N)]
-for _ in range(int(input('간선의 개수 : '))):
-    u, v = map(int,input('인접한 두 정점 입력 ex) 1 2 : ').split())
-    u -= 1; v -= 1 #노드번호 맞추기 위해서 -1
-    #새로운 간선 정보를 삽입
-    Graph[u].append(v)
-    Graph[v].append(u)
+for _ in range(int(input("간선의 개수 : "))):
+    u, v, w = map(int, input("인접한 두 정점 입력 ex) 1 2 : ").split())
+    u -= 1
+    v -= 1  # 노드번호 맞추기 위해서 -1
+    # 새로운 간선 정보를 삽입
+    Graph[u].append((v, w))
+    Graph[v].append((u, w))
+
 
 print_Adjlist(Graph)
--- 2nd/mazeSearch.py
+++ 2nd/mazeSearch.py
@@ -1,56 +1,55 @@
 from collections import deque
 
-BOARD = [[1,0,1,1,1,1],
-         [1,0,1,0,1,0],
-         [1,0,1,0,0,1],
-         [1,1,1,0,1,1]]
- 
-start = (0,0) #출발점
-end = (3,5) #도착점
+BOARD = [[1, 0, 1, 1, 1, 1], [1, 0, 1, 0, 1, 0], [1, 0, 1, 0, 0, 1], [1, 1, 1, 0, 1, 1]]
 
-#BFS로 구현한 미로탐색 함수
-def mazeSearch(board,start,end):
-    #행,열 개수 구함
+start = (0, 0)  # 출발점
+end = (3, 5)  # 도착점
+
+
+# BFS로 구현한 미로탐색 함수
+def mazeSearch(board, start, end):
+    # 행,열 개수 구함
     R = len(board) - 1
     C = len(board[0]) - 1
 
-    #상하좌우 4방향 이동에 사용할 배열
-    dx = [1,-1,0,0]
-    dy = [0,0,1,-1]
-    
-    #방문처리에 사용할 배열
-    visited = [[False for _ in range(C+1)] for _ in range(R+1)]
-    #시작점 방문처리
+    # 상하좌우 4방향 이동에 사용할 배열
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    # 방문처리에 사용할 배열
+    visited = [[False for _ in range(C + 1)] for _ in range(R + 1)]
+    # 시작점 방문처리
     visited[start[0]][start[1]] = True
 
     queue = deque()
     queue.append(start)
-    
-    while(queue):
-        x,y = queue.popleft()
-        #도착점에 방문했으면
-        if (x,y) == (end):
-            #탐색 종료
+
+    while queue:
+        x, y = queue.popleft()
+        # 도착점에 방문했으면
+        if (x, y) == (end):
+            # 탐색 종료
             print("종점 도착 가능")
             return True
-        #4방향으로 이동한 좌표에 대해 탐색할 것
+        # 4방향으로 이동한 좌표에 대해 탐색할 것
         for i in range(4):
-            #tx,ty는 순서대로
-            #(x+1,y),(x-1,y),(x,y+1),(x,y-1)
+            # tx,ty는 순서대로
+            # (x+1,y),(x-1,y),(x,y+1),(x,y-1)
             tx = x + dx[i]
             ty = y + dy[i]
 
-            #보드 사이즈를 넘어가는 경우
-            if tx < 0 or tx > R or ty < 0 or ty > C :
+            # 보드 사이즈를 넘어가는 경우
+            if tx < 0 or tx > R or ty < 0 or ty > C:
                 continue
 
-            #방문하지 않았고, 접근가능(값 : 1)한 경우
+            # 방문하지 않았고, 접근가능(값 : 1)한 경우
             if not visited[tx][ty] and board[tx][ty] == 1:
-                queue.append((tx,ty))
+                queue.append((tx, ty))
                 visited[tx][ty] = True
-    
+
     # while문 탈출 == 종점 도착 못한채 탐색 종료됨
     print("종점 도착 불가능")
     return False
 
-mazeSearch(BOARD,start,end)
+
+mazeSearch(BOARD, start, end)
